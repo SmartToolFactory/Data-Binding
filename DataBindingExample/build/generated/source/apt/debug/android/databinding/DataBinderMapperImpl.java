2a868a341c156390c531090990a32006835c9767
trbibi <trbilgisayarbilimleri@gmail.com>
2019-10-23 00:30:03 +0300

    gitignore added at root

--- DataBindingExample/build/generated/source/apt/debug/android/databinding/DataBinderMapperImpl.java
+++
@@ -1,7 +0,0 @@
-package android.databinding;
-
-public class DataBinderMapperImpl extends MergedDataBinderMapper {
-  DataBinderMapperImpl() {
-    addMapper(new com.troy379.databindingexample.DataBinderMapperImpl());
-  }
-}
